4ead91fb4d5c4082f6f5a695244ab6a4122061cf
Umit Caybas <umit.caybas@nextpracticehealth.com>
2022-05-16 21:05:06 +1000

    added 2nd and 3rd test scenario coverage

--- pages/homePage.js
+++ pages/homePage.js
@@ -14,17 +14,34 @@ const buttons = {
 }
 
 // Clicks on each locator that is passed as an array
-const clickLocators = locators => {
+const addItems = (...locators) => {
     locators.forEach(locator => {
         I.click(locator);
     })
 }
 
+// Clicks on each locator that is passed as an array
+const deleteItems = (...locators) => {
+    locators.forEach(locator => {
+        I.click(locator);
+    })
+}
 
+const refreshItems = () => {
+    I.click(buttons.refreshItems)
+}
+
+// Validates the total item count is equal to the param
+const validateTotalItemCount = async expectedCount => {
+    expect(await I.grabTextFrom(badges.itemsCount)).to.equal(expectedCount);
+}
 
 module.exports = {
     badges,
     buttons,
-    clickLocators
+    addItems,
+    refreshItems,
+    deleteItems,
+    validateTotalItemCount
 }
 
--- tests/home_test.js
+++ tests/home_test.js
@@ -1,30 +1,38 @@
 const expect = require("chai").expect;
 
-Feature('home');
+Feature('Counter App');
 
-Scenario('First scenario - Add 2 of each item and verify item count of 8', async ({ I, homePage }) => {
+xScenario('First scenario - Add 2 of each item and verify total item count is 8', async ({ I, homePage }) => {
     await I.amOnPage("/");
-    // I.click(homePage.buttons.addItem)
-    // I.click(homePage.buttons.addItem)
-    // I.click(homePage.buttons.addItem.at(2))
-    // I.click(homePage.buttons.addItem.at(2))
-    // I.click(homePage.buttons.addItem.at(3))
-    // I.click(homePage.buttons.addItem.at(3))
-    // I.click(homePage.buttons.addItem.at(4))
-    // I.click(homePage.buttons.addItem.at(4))
     const itemsToAdd = [
         homePage.buttons.addItem,
         homePage.buttons.addItem.at(2),
         homePage.buttons.addItem.at(3),
         homePage.buttons.addItem.at(4)
     ]
-    await homePage.clickLocators(itemsToAdd);
-    await homePage.clickLocators(itemsToAdd);
-
+    await homePage.addItems(itemsToAdd);
+    await homePage.addItems(itemsToAdd);
 
     const itemCount = await I.grabTextFrom(homePage.badges.itemsCount);
-    console.log(itemCount)
-    expect(itemCount).to.equal(8)
-
-    I.wait(5);
+    expect(itemCount).to.equal("8", `Expected 8 items in total but there was ${itemCount}`);
+    console.log("all the items are added successfully")
 });
+
+Scenario('Second scenario - Validate refreshing the cart will reset item count', async ({ I, homePage }) => {
+    await I.amOnPage("/");
+    homePage.validateTotalItemCount("0");
+    homePage.addItems(homePage.buttons.addItem);
+    homePage.validateTotalItemCount("1");
+    homePage.refreshItems();
+    homePage.validateTotalItemCount("0");
+});
+
+Scenario('Third scenario - Delete item and verify total item count correctly updates', async ({ I, homePage }) => {
+    await I.amOnPage("/");
+    homePage.validateTotalItemCount("0");
+    homePage.addItems(homePage.buttons.addItem);
+    homePage.validateTotalItemCount("1");
+    homePage.deleteItems(homePage.buttons.deleteItem)
+    homePage.validateTotalItemCount("0");
+    console.log("item is deleted successfully");
+})
